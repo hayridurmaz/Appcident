19e573f702374af6bacf96a9d2a9d86740fffc21
batuhanmert <33976236+batuhanmert@users.noreply.github.com>
2018-10-25 13:47:07 +0300

    TRYING to process sensor data

---
+++ app/src/main/java/tr/edu/tedu/appcident/SensorActivity.java
@@ -0,0 +1,93 @@
+package tr.edu.tedu.appcident;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Debug;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.Console;
+
+public class SensorActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mPressure;
+    private Sensor mHeat;
+    private Sensor mAcceleration;
+    private Sensor mGyroscope;
+    private Sensor mLight;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mHeat = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        mAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mHeat, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float currentValue = event.values[0];
+
+        int sensorType = event.sensor.getType();
+        String name1 = event.sensor.getName();
+        TextView textt = (TextView) findViewById(R.id.label_light);
+        mLight.setText(getResources().getString(
+                R.string.label_light1, currentValue));
+
+
+
+        // Do something with this sensor data.
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mHeat, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+}
