1a7bb208af5f60dbc92c414123a30ed7168b87c3
Batuhan Mert Karabulut <batuhanmertkarabulut@gmail.com>
2018-11-13 20:19:45 +0300

    Sensör data işleme ve database bağlantısı birleştirildi.
    SensorActivity bizim ana activity miz. Sensör dataları burda işleniyor ve şuanki hali database e bağlanmış durumda.

--- app/src/main/java/tr/edu/tedu/appcident/MainActivity.java
+++ app/src/main/java/tr/edu/tedu/appcident/MainActivity.java
@@ -1,19 +1,15 @@
 package tr.edu.tedu.appcident;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.google.firebase.FirebaseError;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,22 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("hello").child("greetings");
+        DatabaseReference myRef = database.getReference("Users").child("User1");
        // myRef.setValue("sa","as");
 
-
-        final TextView t= (TextView) findViewById(R.id.helloworld);
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                t.setText(dataSnapshot.getValue().toString());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
 
     }
--- app/src/main/java/tr/edu/tedu/appcident/SensorActivity.java
+++ app/src/main/java/tr/edu/tedu/appcident/SensorActivity.java
@@ -1,32 +1,51 @@
 package tr.edu.tedu.appcident;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Environment;
 import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
-public class SensorActivity extends Activity implements SensorEventListener {
+public class SensorActivity extends Activity implements SensorEventListener/*, View.OnClickListener, SurfaceHolder.Callback*/ {
     private static final int INTENTCAPTURE_VIDEO_ACTIVITY_REQUEST_CODE =200 ;
     private SensorManager mSensorManager;
     private Sensor mPressure;
@@ -36,6 +55,8 @@ public class SensorActivity extends Activity implements SensorEventListener {
     private Sensor mLight;
     private Sensor mRotation;
 
+    private Button settingsButton;
+
     private float deltaX = 0;
     private float deltaY = 0;
     private float deltaZ = 0;
@@ -43,18 +64,43 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
     private float vibrateThreshold = 0;
 
-
+    MediaRecorder recorder;
+    SurfaceHolder holder;
+    boolean recording = false;
 
     public Vibrator v;
 
     TextView output;
+
+    int YOUR_REQUEST_CODE = 200; // could be something else..
 
 
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+/*
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        recorder = new MediaRecorder();
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) //check if permission request is necessary
+        {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, YOUR_REQUEST_CODE);
+        }
+
+        initRecorder();
+*/
         setContentView(R.layout.activity_sensor);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users").child("User1");
+
+        settingsButton = (Button)findViewById(R.id.button1);
 
         output = (TextView) findViewById(R.id.label_light);
         // Get an instance of the sensor service, and use that to get an instance of
@@ -69,11 +115,6 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
 
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 
     @Override
@@ -123,12 +164,12 @@ public class SensorActivity extends Activity implements SensorEventListener {
                         + Math.pow(loY, 2)
                         + Math.pow(loZ, 2));
 
-                DecimalFormat precision = new DecimalFormat("0.00");// Telefona yüklerken virgül yap
+                DecimalFormat precision = new DecimalFormat("0,00");// Telefona yüklerken virgül yap
                 double ldAccRound = Double.parseDouble(precision.format(loAccelerationReader));
 
                 if (ldAccRound > 0.3d && ldAccRound < 0.5d) {
                     textt.setText("Düştü");
-
+                    //emergencyMode();
                 }
 
                 break;
@@ -254,4 +295,71 @@ public class SensorActivity extends Activity implements SensorEventListener {
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
+/*
+    protected void emergencyMode () {
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.CameraView);
+        holder = cameraView.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        cameraView.setClickable(true);
+        cameraView.setOnClickListener(this);
+    }
+
+    private void initRecorder() {
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+        CamcorderProfile cpHigh = CamcorderProfile
+                .get(CamcorderProfile.QUALITY_HIGH);
+        recorder.setProfile(cpHigh);
+        recorder.setOutputFile("/sdcard/videocapture_example.mp4");
+        recorder.setMaxDuration(50000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(holder.getSurface());
+
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+            initRecorder();
+            prepareRecorder();
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+    }
+    */
 }
