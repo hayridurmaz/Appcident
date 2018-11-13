fe10651efb4cab0507d687cf711fd6c49946f7f3
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-11-13 06:32:05 +0300

    video record (not working)

--- app/src/main/java/tr/edu/tedu/appcident/SensorActivity.java
+++ app/src/main/java/tr/edu/tedu/appcident/SensorActivity.java
@@ -3,21 +3,31 @@ package tr.edu.tedu.appcident;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Debug;
+import android.os.Environment;
 import android.os.Vibrator;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.io.Console;
+import java.io.File;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 public class SensorActivity extends Activity implements SensorEventListener {
+    private static final int INTENTCAPTURE_VIDEO_ACTIVITY_REQUEST_CODE =200 ;
     private SensorManager mSensorManager;
     private Sensor mPressure;
     private Sensor mHeat;
@@ -37,6 +47,8 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
     public Vibrator v;
 
+    TextView output;
+
 
 
     @Override
@@ -44,6 +56,7 @@ public class SensorActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
 
+        output = (TextView) findViewById(R.id.label_light);
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -110,11 +123,12 @@ public class SensorActivity extends Activity implements SensorEventListener {
                         + Math.pow(loY, 2)
                         + Math.pow(loZ, 2));
 
-                DecimalFormat precision = new DecimalFormat("0,00");
+                DecimalFormat precision = new DecimalFormat("0.00");// Telefona yüklerken virgül yap
                 double ldAccRound = Double.parseDouble(precision.format(loAccelerationReader));
 
                 if (ldAccRound > 0.3d && ldAccRound < 0.5d) {
                     textt.setText("Düştü");
+
                 }
 
                 break;
@@ -131,6 +145,93 @@ public class SensorActivity extends Activity implements SensorEventListener {
         }
 
         // Do something with this sensor data.
+    }
+
+    private Uri getOutputMediaFileUri(int type){
+
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private File getOutputMediaFile(int type){
+
+        // Check that the SDCard is mounted
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraVideo");
+
+
+        // Create the storage directory(MyCameraVideo) if it does not exist
+        if (! mediaStorageDir.exists()){
+
+            if (! mediaStorageDir.mkdirs()){
+
+
+                output.setText("Failed to create directory MyCameraVideo.");
+
+                Toast.makeText(SensorActivity.this, "Failed to create directory MyCameraVideo.",
+                        Toast.LENGTH_LONG).show();
+
+                Log.d("MyCameraVideo", "Failed to create directory MyCameraVideo.");
+                return null;
+            }
+        }
+
+
+        // Create a media file name
+
+        // For unique file name appending current timeStamp with file name
+        java.util.Date date= new java.util.Date();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(date.getTime());
+
+        File mediaFile;
+
+        if(type == MEDIA_TYPE_VIDEO) {
+
+            // For unique video file name appending current timeStamp with file name
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // After camera screen this code will excuted
+
+        if (requestCode == INTENTCAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+
+            if (resultCode == RESULT_OK) {
+
+                output.setText("Video File : " +data.getData());
+
+                // Video captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Video saved to:" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+
+            } else if (resultCode == RESULT_CANCELED) {
+
+                output.setText("User cancelled the video capture.");
+
+                // User cancelled the video capture
+                Toast.makeText(this, "User cancelled the video capture.",
+                        Toast.LENGTH_LONG).show();
+
+            } else {
+
+                output.setText("Video capture failed.");
+
+                // Video capture failed, advise user
+                Toast.makeText(this, "Video capture failed.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
