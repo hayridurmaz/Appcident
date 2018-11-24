5ec6fcd7aca35c2ed91d372a3772f57c2e4da6f6
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-11-24 06:04:54 +0300

    database model made better

--- app/src/main/java/tr/edu/tedu/appcident/SettingsActivity.java
+++ app/src/main/java/tr/edu/tedu/appcident/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +14,11 @@ import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toolbar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends Activity {
     String phoneNumber1;
@@ -26,6 +30,7 @@ public class SettingsActivity extends Activity {
     EditText phoneInput3;
 
     TextView IMEITextView;
+    TextView AddedPhones;
     String IMEINumber;
 
 
@@ -47,7 +52,7 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference("Users");
         // myRef.setValue("sa","as");
 
@@ -65,6 +70,26 @@ public class SettingsActivity extends Activity {
         phoneInput3 = (EditText) findViewById(R.id.phoneInput3);
 
         submitButton = (Button) findViewById(R.id.submit);
+        AddedPhones=(TextView) findViewById(R.id.addedPhones);
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                if(dataSnapshot.child(IMEINumber).exists()){
+                    AddedPhones.setText("Numbers that you have added: \n-"+dataSnapshot.child(IMEINumber).child("number1").getValue()+"\n-"+dataSnapshot.child(IMEINumber).child("number2").getValue()+"\n-"+dataSnapshot.child(IMEINumber).child("number3").getValue());
+                }
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,8 +114,8 @@ public class SettingsActivity extends Activity {
                 }
                 if (phoneNumber3.length() == 11 && phoneNumber2.length() == 11 && phoneNumber1.length() == 11) {
                     showToast("Now your emergency list is ready!!");
-                    User user = new User(phoneNumber1, phoneNumber2, phoneNumber3);
-                    myRef.child("User1").setValue(user);
+                    User user = new User(phoneNumber1, phoneNumber2, phoneNumber3,IMEINumber);
+                    myRef.child(IMEINumber).setValue(user);
                     Intent intent = new Intent(SettingsActivity.this, SensorActivity.class);
                     startActivity(intent);
                 }
--- app/src/main/java/tr/edu/tedu/appcident/User.java
+++ app/src/main/java/tr/edu/tedu/appcident/User.java
@@ -1,11 +1,12 @@
 package tr.edu.tedu.appcident;
 
 public class User {
-    public String number1, number2, number3;
+    public String number1, number2, number3, imei;
 
-    public User (String number1, String number2, String number3){
+    public User (String number1, String number2, String number3, String imei){
         this.number1 = number1;
         this.number2 = number2;
         this.number3 = number3;
+        this.imei=imei;
     }
 }
